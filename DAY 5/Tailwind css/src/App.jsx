fe47679bea6d8db7b205a320faff719c0de908d3
dimassamiaji <dimassamji@gmail.com>
2023-12-15 11:34:43 +0700

    update Tailwind css

---
+++ DAY 5/Tailwind css/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavbarComponents from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <NavbarComponents />
+      <h1 className=" text-3xl font-[900] underline">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ DAY 5/Tailwind css/src/components/navbar.jsx
@@ -0,0 +1,8 @@
+function NavbarComponents() {
+  return (
+    <div className=" bg-black w-full py-6 text-white px-4">
+      <h1 className=" text-2xl font-extrabold">Logo</h1>
+    </div>
+  );
+}
+export default NavbarComponents;
